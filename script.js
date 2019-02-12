18105081f472cd9423a361d3d6fdb817e663eee8
paulinaboro <paulina.borowiecka554@gmail.com>
2019-02-12 23:06:29 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// ---------------COLOR PALETTE TIPS ----------------
+
+//  function colorPick(event)
+
+// function setBaseColor- this function should also set the other colors
+
+// function calculateColors (base)- this function should calculate matching colors and match 4 of them(through different method like:)
+
+// comment: we have to figure out how to represent the colors :
+// 1.we could have an object of {r, g, b}
+// 2.we could habe an object of {h, s ,l}
+
+// ---------function picks the color--------------
+function colorPick() {
+  let chosenColor = document.getElementById("myColor").value;
+  document.getElementById("demo").innerHTML = chosenColor;
+}
+
+// ---------function sets the picked/base color--------------
+function setBaseColor() {}
+
+// -----------function calculate 4 matching colors----------------
+function calculateMatchingColors() {}
+
+colorPick();
+
+function myTest() {}
+
+function myTest() {}
