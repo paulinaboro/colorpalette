6d2cbcd54691e6e81b8f48682c76867d632125df
paulinaboro <paulina.borowiecka554@gmail.com>
2019-02-12 23:49:36 +0100

    Another Commit

--- script.js
+++ script.js
@@ -1,33 +1,36 @@
 // ---------------COLOR PALETTE TIPS ----------------
 
-//  function colorPick(event)
-
-// function setBaseColor- this function should also set the other colors
-
-// function calculateColors (base)- this function should calculate matching colors and match 4 of them(through different method like:)
-
-// comment: we have to figure out how to represent the colors :
+// Create a function colorPick(event)
+// Function setBaseColor- this function should also set the other colors
+// Function calculateColors (base)- this function should calculate matching colors and match 4 of them(through different methods).
+// Comment: we have to figure out how to represent the colors.
 // 1.we could have an object of {r, g, b}
-// 2.we could habe an object of {h, s ,l}
+// 2.we could have an object of {h, s ,l}
 
 // ---------function picks the color--------------
-function colorPick() {
+
+function colorPick(value) {
+  let colorPick = "";
   let chosenColor = document.getElementById("myColor").value;
   document.getElementById("demo").innerHTML = chosenColor;
+  return chosenColor;
 }
+function setBackgroundColor(){
+  document.querySelector(chosenColor).setAttribute("style","background-color:`$colorPick}"
+!important"`);
+}
+document.getElementById("selected").innerHTML = setBackgroundColor(chosenColor);
+
 
 // ---------function sets the picked/base color--------------
 function setBaseColor() {}
 
-// -----------function calculate 4 matching colors----------------
-function calculateMatchingColors() {}
-
 colorPick();
+setBaseColor();
 
-function myTest() {}
+//-----------function calculate 4 matching colors----------------
+// function calculateMatchingColors() {}
 
-function myTest() {}
-function myTest() {}
-function myTest() {}
-function vamosPlaya() {}
-function vamosPlaya() {}
+// let baseColor = document.getElementById(chosenColor).value;
+//   document.querySelector.(".selected").setAttribute("style",
+//     "background-color:`${colorPick} !important"`);
