42a1a14e48b14716e2d5189dccef7732cf7828cd
paulinaboro <paulina.borowiecka554@gmail.com>
2019-02-13 12:28:32 +0100

    Hex display works

--- script.js
+++ script.js
@@ -17,9 +17,12 @@ let head = document.querySelector("#head");
 
 let selectedColor = document.querySelector(".color1");
 
+let hex_color = document.querySelector(".hex_color");
+
 // background color w color1 will be = to input- head id
 color1.style.backgroundColor = head.value;
 
 head.addEventListener("input", function(pickedColor) {
   color1.style.backgroundColor = pickedColor.target.value;
+  hex_color.textContent = pickedColor.target.value;
 });
